b5d4a93c9dc4fdaa8e46717bc3191129a80c408f
somm12 <thals8744@naver.com>
2021-07-26 14:38:38 +0900

    마이페이지 이미지 출력 부분 완료, post id data 가져오기 시작

--- log/static/js/mypage.js
+++ log/static/js/mypage.js
@@ -7,59 +7,60 @@ $(document).ready(function () {
   for (var a = 0; a < 7; a++) {
     $(".week").append('<div class="day"></div>');
   }
+  $(".week:nth-child(" + 1 + ")").css({});
   /*프로필 레벨 */
-
+  console.log(post_count);
   var user_level = document.querySelector(".myprofile_rank");
   var next_level = document.querySelector(".next_level");
   var left_num = document.querySelector(".left_num_text");
-  var count = "{{count|safe}}";
-  if (count >= 0 && count < 15) {
+
+  if (post_count >= 0 && post_count < 15) {
     user_level.innerHTML = "등급 : Bronze";
     next_level.innerHTML = "Silver";
-    left_num.innerHTML = "까지 앞으로" + 15 - count + "개!";
-    $(".left_name").css({
+
+    left_num.innerHTML = "까지 앞으로 " + (15 - post_count) + "개!";
+    $(".left_num_text").css({
       color: "#435f7a",
     });
-  } else if (count >= 15 && count < 30) {
+  } else if (post_count >= 15 && post_count < 30) {
     user_level.innerHTML = "등급 : Silver";
     next_level.innerHTML = "Gold";
-    left_num.innerHTML = "까지 앞으로" + 30 - count + "개!";
-    $(".left_name").css({
+    left_num.innerHTML = "까지 앞으로 " + (30 - post_count) + "개!";
+    $(".left_num_text").css({
       color: "#eb9a01",
     });
-  } else if (count >= 30 && count < 45) {
+  } else if (post_count >= 30 && post_count < 45) {
     user_level.innerHTML = "등급 : Gold";
     next_level.innerHTML = "Platinum";
-    left_num.innerHTML = "까지 앞으로" + 45 - count + "개!";
-    $(".left_name").css({
+    left_num.innerHTML = "까지 앞으로 " + (45 - post_count) + "개!";
+    $(".left_num_text").css({
       color: "#2a9e7b",
     });
-  } else if (count >= 45 && count < 60) {
+  } else if (post_count >= 45 && post_count < 60) {
     user_level.innerHTML = "등급 : Platinum";
     next_level.innerHTML = "Diamond";
-    left_num.innerHTML = "까지 앞으로" + 60 - count + "개!";
-    $(".left_name").css({
+    left_num.innerHTML = "까지 앞으로 " + (60 - post_count) + "개!";
+    $(".left_num_text").css({
       color: "#00b4fc",
     });
-  } else if (count >= 60 && count < 75) {
+  } else if (post_count >= 60 && post_count < 75) {
     user_level.innerHTML = "등급 : Diamond";
     next_level.innerHTML = "Ruby";
-    left_num.innerHTML = "까지 앞으로" + 75 - count + "개!";
-    $(".left_name").css({
+    left_num.innerHTML = "까지 앞으로 " + (75 - post_count) + "개!";
+    $(".left_num_text").css({
       color: "#ff0061",
     });
-  } else if (count >= 75) {
+  } else if (post_count >= 75) {
     user_level.innerHTML = "등급 : Ruby";
     next_level.innerHTML = "Silver";
     left_num.innerHTML = "Ruby";
-    $(".left_name").css({
+    $(".left_num_text").css({
       color: "#ff0061",
     });
   }
-
+  console.log(dates);
   /*잔디밭 색칠하기*/
-  /*
-  var dates = "{{dates|safe}}"; // [월, 일 ,월, 일 ...]형식이며 두 개씩세트
+
   var count = "{{count|safe}}";
   //각 1월, 2월, 3월, 4월 .. 날짜 수 배열.
   var date_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -85,17 +86,29 @@ $(document).ready(function () {
     for (var k = 0; k < month_sum; k++) {
       sum += date_in_month[k];
     }
-    var week_nth = sum / 7 + 1; //몇 번째 주차의
-    var final_nth = (sum % 7) + mypost_dates[i + 1]; //몇 번째 일에 post올림을 기록.
+    console.log(sum / 7);
+    var final_nth = 0;
+    var week_nth = Math.floor(sum / 7) + Math.floor(mypost_dates[i + 1] / 7); //몇 번째 주차의
+    if ((sum % 7) + (mypost_dates[i + 1] % 7) > 7) {
+      week_nth += 1;
+      final_nth = (sum % 7) + (mypost_dates[i + 1] % 7) - 7;
+      console.log(1);
+    } else {
+      final_nth = (sum % 7) + (mypost_dates[i + 1] % 7);
+      console.log(sum % 7);
+      console.log(mypost_dates[i + 1]);
+      console.log(final_nth);
+    }
+
     $(
       ".week:nth-child(" + week_nth + ") .day:nth-child(" + final_nth + ")"
     ).css({
-      "background-color": "#a1d177;",
+      "background-color": "#a1d177",
     });
   }
 
   console.log(mypost_dates);
-*/
+
   //만약 기록했던 글로 이동하고 싶다면,? a태그 href 바꾸는 법.
   // $("#linkMyBlog").attr("href", "http://ozit.tistory.com")
 });
