0ee938372cc400b1538b185ea798611f5075809a
somm12 <thals8744@naver.com>
2021-08-11 21:56:42 +0900

    오타 수정

--- greenlog/settings.py
+++ greenlog/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware’,
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'greenlog.urls'
