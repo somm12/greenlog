35763cb8896bb19c7b163fd2cfd88c26b26ce485
somm12 <thals8744@naver.com>
2021-08-12 12:29:35 +0900

    password max_length 100으로 수정

--- log/models.py
+++ log/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=10,default='')
     nickname = models.CharField(max_length=10,primary_key=True)
-    password =models.CharField(max_length=10,default='')
+    password =models.CharField(max_length=100,default='')
     profile = models.ImageField(upload_to='images/', height_field=None, width_field=None, max_length=None, blank=True, null=True)
     Member =models.CharField(max_length=10,default='')
 
